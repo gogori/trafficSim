94f0a07e127dedb3920c7cc9b1ff07f12050a94e
gogori <jinqi1989@gmail.com>
2015-01-21 11:46:24 -0500

    Initial Commit

---
+++ trafficSim/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  trafficSim
+//
+//  Created by Jinqi Fang on 1/21/15.
+//  Copyright (c) 2015 Jinqi Fang. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
